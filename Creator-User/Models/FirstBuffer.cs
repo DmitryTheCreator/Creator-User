7ffc25f85af334ae833435e8307c4fb4ee65e856
DmitryTheCreator <dmitryzamorev@gmail.com>
2022-10-17 21:31:42 +0500

    Добавьте файлы проекта.

---
+++ Creator-User/Models/FirstBuffer.cs
@@ -0,0 +1,21 @@
+﻿namespace Creator_User.Model
+{
+    internal class FirstBuffer
+    {
+        private string message;
+        private int character_index;
+
+        public void Write(string message)
+        {
+            this.message = message + '\n';
+            character_index = 0;
+        }
+
+        public char Read()
+        {
+            if (message.Length == 0 || message == "\n")
+                throw new System.Exception("Буфер пуст!");
+            return message[character_index++];
+        }
+    }
+}
---
+++ Creator-User/Models/SecondBuffer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+
+namespace Creator_User.Model
+{
+    internal class SecondBuffer
+    {
+        private List<string> messages;
+        private int message_index;
+        private int character_index;
+
+
+        public SecondBuffer(int size = 3)
+        {        
+            if (size < 2)
+                throw new System.Exception("Минимальный размер второго буфера - 2 сообщения!");
+            messages = new List<string>(size);
+            message_index = 0;
+            character_index = 0;
+        }
+
+        public void Write(char character)
+        {
+            if (character == '\n')
+            {
+                if (message_index == messages.Count - 1)
+                    messages.RemoveAt(0);
+                else
+                    message_index++;
+                character_index = 0;
+                messages.Add("");
+            }
+            else
+                messages[message_index] += character;
+        }
+
+        public char Read()
+        {
+            return messages[message_index][character_index];
+        }
+    }
+}
